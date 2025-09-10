9f4b4f15918bb429a31928429467fe41492d4bd5
Sylvvs <kongsted.simon@gmail.com>
2025-09-10 09:42:03 +0200

    fixed camera and adjusted parallax

--- scenes/playercamera.gd
+++ scenes/playercamera.gd
@@ -1,6 +1,5 @@
 extends Camera2D
 
 func _process(delta: float) -> void:
-	global_position.x = get_parent().global_position.x - get_viewport_rect().size.x / 4
-	position.y = -get_viewport_rect().size.y / 4
+	pass
 	
