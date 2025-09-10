10614a2dd909c3da45ad14394b4d5e9d66cff7a6
Ulveboss <mikkel@iploug.dk>
2025-09-10 10:25:27 +0200

    idle animation for enemy
    idle

--- scenes/player.gd
+++ scenes/player.gd
@@ -22,7 +22,6 @@ func _physics_process(delta):
 		
 	if Input.is_action_pressed("ui_up") and is_on_floor():
 		velocity.y = -JUMP;
-		velocity.x = SPEED;
 		
 	velocity.y = velocity.y + GRAV * delta;
 	move_and_slide()
