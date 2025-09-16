9968dbb36bbb5475b973578ac60249fc83b9f4ee
Sylvvs <kongsted.simon@gmail.com>
2025-09-16 13:24:34 +0200

    okay now npcs actually work again lmfao

--- scripts/DialogueManager.gd
+++ scripts/DialogueManager.gd
@@ -76,7 +76,7 @@ func _typewriter(label: RichTextLabel, lines: Array) -> void:
 		
 		for i in range(line.length()):
 			label.text += line[i]
-			if Input.is_action_pressed("ui_down") and i > 10:
+			if Input.is_action_pressed("interact") and i > 10:
 				label.text = line
 				break
 			await get_tree().create_timer(0.03).timeout
@@ -84,7 +84,7 @@ func _typewriter(label: RichTextLabel, lines: Array) -> void:
 		var waiting_for_input = true
 		while waiting_for_input:
 			await get_tree().process_frame
-			if Input.is_action_just_pressed("ui_down"):
+			if Input.is_action_just_pressed("interact"):
 				waiting_for_input = false
 
 
@@ -104,7 +104,7 @@ func _process(delta):
 			dialogue_closed_recently = false
 			dialogue_close_cooldown = 0.2
 	elif dialogue_box and dialogue_waiting_close:
-		if Input.is_action_just_pressed("ui_down"):
+		if Input.is_action_just_pressed("interact"):
 			_close_dialogue()
 			dialogue_waiting_close = false
 
