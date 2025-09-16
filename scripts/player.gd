b4604ecc4f8732f51dafaf8b34a3a85e8e052917
Ulveboss <mikkel@iploug.dk>
2025-09-16 12:25:32 +0200

    input map

--- scripts/player.gd
+++ scripts/player.gd
@@ -52,12 +52,12 @@ func _physics_process(delta):
 	if  is_knockback:
 		velocity = knockback_velocity
 		velocity.y += GRAV * delta
-	elif Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("left"):
 		if not is_attacking:
 			sprite.flip_h = true
 			sword.scale.x = -1
 		velocity.x = -SPEED;
-	elif Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("right"):
 		if not is_attacking:
 			sprite.flip_h = false
 			sword.scale.x = 1
