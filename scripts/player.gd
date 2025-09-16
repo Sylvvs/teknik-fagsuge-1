1c29850aa795c0d0ee36840e8ada0b4cfec780ae
Ulveboss <mikkel@iploug.dk>
2025-09-16 12:25:03 +0200

    input map

--- scripts/player.gd
+++ scripts/player.gd
@@ -65,7 +65,7 @@ func _physics_process(delta):
 	else:
 		velocity.x = 0
 		
-	if Input.is_action_pressed("ui_up") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = -JUMP;
 		
 	velocity.y = velocity.y + GRAV * delta;
