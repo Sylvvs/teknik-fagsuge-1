0be6a7724921d66a2ff8ff18c49e2b5fb01d2d49
Maffeq <Victor.scharf.heinemann@outlook.dk>
2025-09-17 17:19:59 +0200

    Golem BONK Melee virker nu

--- scripts/MeleeAttack.gd
+++ scripts/MeleeAttack.gd
@@ -7,3 +7,9 @@ func enter():
 func transition():
 	if owner.direction.length() > 30:
 		get_parent().change_state("Follow")
+
+
+func _on_melee_attack_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		body.take_damage(1, global_position) 
+		print(body.take_damage)
