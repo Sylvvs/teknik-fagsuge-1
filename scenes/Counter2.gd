00247532a58a6b350277c44d49115d975d3c3738
Maffeq <Victor.scharf.heinemann@outlook.dk>
2025-09-18 21:29:14 +0200

    LUCIFER VIRKER IKKKEKEKEKEKE

--- scenes/Counter2.gd
+++ scenes/Counter2.gd
@@ -3,7 +3,11 @@ extends LuciferState
 func enter():
 	super.enter()
 	counter_move()
+	set_physics_process(true)
 
+func exit():
+	super.exit()
+	set_physics_process(false)
 
 func counter_move():
 	if not player: 
@@ -13,21 +17,18 @@ func counter_move():
 	
 	# Diagonal offset: right-up or left-up
 	var offset = Vector2(10 * dir, -100) 
-	
+	print(owner.global_position)
 	# Teleport Lucifer
 	owner.global_position += offset
-	
-	
-	await play_animation("attack 1", 1) 
+	print(owner.global_position)
+	play_animation("attack 1", 1) 
 func play_animation(anim_name: String, speed: float = 1.0):
 	animation_player.play(anim_name, -1.0, speed)
 	await animation_player.animation_finished
-	if owner.direction.length() < 45:
-		play_animation(anim_name, speed)
-		
+	#play_animation(anim_name, speed)
+	
 func transition():
 	if animation_player.is_playing():
 		return
 	else:
-		if owner.direction.length() > 45:
-			get_parent().change_state("LuciferFollow")
+		get_parent().call_deferred("change_state", "LuciferAttack1")
--- scripts/LuciferFollow.gd
+++ scripts/LuciferFollow.gd
@@ -1,5 +1,6 @@
 extends LuciferState
 var time = 0
+var has_countered : bool = false
 func enter():
 	super.enter()
 	owner.set_physics_process(true)
@@ -8,18 +9,25 @@ func enter():
 func exit():
 	super.exit()
 	owner.set_physics_process(false)
-	
+	has_countered = false
+
+func transition_to_counterhit():
+	get_parent().call_deferred("change_state", "CounterHit")
+
 func transition():
 	var distance = owner.direction.length()
 	if distance < 45:
-		get_parent().change_state("LuciferAttack1")
-	#if time > 10:
-		#get_parent().change_state("CounterHit")
-	
+		get_parent().call_deferred("change_state", "LuciferAttack1")
+		
 func _process(delta: float) -> void:
 	var distance = owner.direction.length()
-	if distance >= 0 and distance <= 500: 
+	if distance >= 0 and distance <= 1000: 
 		time += delta
 	else:
 		time = 0
-	
+	if time > 5 and not has_countered:
+		has_countered = true
+		transition_to_counterhit()
+		time = 0
+	if time > 7: 
+		time = 0
