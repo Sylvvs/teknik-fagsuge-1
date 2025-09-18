bf2089893f30d4d27353b64b0b5e0c6411384690
Sylvvs <kongsted.simon@gmail.com>
2025-09-18 18:33:43 +0200

    changed dash and fixed cave

--- scripts/LuciferAttack1.gd
+++ scripts/LuciferAttack1.gd
@@ -1,14 +1,22 @@
 extends LuciferState
 @onready var lucifer = $"../.."
+var dash_time_max = 0.075;
+var dash_time = dash_time_max
+
 func enter():
 	super.enter()
 	set_physics_process(true)
 	await play_animation("attack 1", 1)
 	
 func attack():
-	lucifer.velocity.x += 35000 * lucifer.aniplayrot.scale.x
-	lucifer.move_and_slide()
-	print("hej")
+	dash_time = 0
+
+func _process(delta: float) -> void:
+	if dash_time < dash_time_max:
+		lucifer.velocity.x += 300 * lucifer.aniplayrot.scale.x
+		lucifer.move_and_slide()
+	dash_time += delta
+
 func play_animation(anim_name: String, speed: float = 1.0,):
 	animation_player.play(anim_name, -1.0, speed)
 	await animation_player.animation_finished
