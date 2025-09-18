7a3b93d2de2fa4001600d39bb8a97cdde528564b
Ulveboss <mikkel@iploug.dk>
2025-09-18 15:45:41 +0200

    tutorialplace

--- scripts/EnemyDeath.gd
+++ scripts/EnemyDeath.gd
@@ -1,6 +1,11 @@
 extends EnemyState
+
+
 func enter():
 	super.enter()
+	owner.set_process(false)
+	owner.set_physics_process(false)
 	animation_player.play("Death")
 	await animation_player.animation_finished
 	queue_free()
+	
