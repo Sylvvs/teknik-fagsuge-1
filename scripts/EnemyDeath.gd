f09c83b06259f7ba449a9e71d56a0f2ea982a9fc
Maffeq <Victor.scharf.heinemann@outlook.dk>
2025-09-18 16:04:42 +0200

    Update EnemyDeath.gd

--- scripts/EnemyDeath.gd
+++ scripts/EnemyDeath.gd
@@ -3,7 +3,6 @@ extends EnemyState
 @onready var enemy = get_parent().get_parent() as CharacterBody2D
 func enter():
 	super.enter()
-	
 	animation_player.play("Death")
 	await animation_player.animation_finished
 	enemy.queue_free()
