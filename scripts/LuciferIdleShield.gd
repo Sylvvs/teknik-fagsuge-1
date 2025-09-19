4f92d610404d4a4445ec8334c7d7c74a6d33ed76
Maffeq <Victor.scharf.heinemann@outlook.dk>
2025-09-19 15:56:16 +0200

    lucifer shield wierd

--- scripts/LuciferIdleShield.gd
+++ scripts/LuciferIdleShield.gd
@@ -1,12 +1,15 @@
 extends LuciferState
-var knockback_dir = (player.global_position - owner.global_position).normalized()
-
+@onready var idle_shield : Area2D = $"../../Idleshield"
 func enter():
 	super.enter()
+	idle_shield.monitoring = true
 	
 
 
 
 func _on_idleshield_area_entered(area: Area2D) -> void:
-	if area.owner and area.owner.is_in_group("player"):
-		player.knockback_velocity = knockback_dir * player.SPEED * 10
+	#if area.owner and area.owner.is_in_group("player"):
+#		player.knockback_velocity = knockback_dir * player.SPEED * 10
+	pass
+func transition():
+	pass
